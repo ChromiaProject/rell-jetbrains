a2e3f441a57e4e784ddfcd76bf5eb26a3938c66e
Mikheil Kezherashvili <misha1024@gmail.com>
2023-08-03 14:36:58 +0400

    Spellchecking for comments and strings

---
+++ src/main/kotlin/net/postchain/rellide/jetbrains/spellchecking/RellSpellcheckingStrategy.kt
@@ -0,0 +1,41 @@
+package net.postchain.rellide.jetbrains.spellchecking
+
+import com.intellij.codeInsight.CodeInsightUtilCore
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.spellchecker.inspections.PlainTextSplitter
+import com.intellij.spellchecker.tokenizer.EscapeSequenceTokenizer
+import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy
+import com.intellij.spellchecker.tokenizer.TokenConsumer
+import com.intellij.spellchecker.tokenizer.Tokenizer
+import net.postchain.rellide.jetbrains.language.RellLanguage
+import net.postchain.rellide.jetbrains.language.psi.RellTypes
+
+class RellSpellcheckingStrategy : SpellcheckingStrategy() {
+    override fun isMyContext(element: PsiElement) = RellLanguage.INSTANCE.`is`(element.language)
+
+    override fun getTokenizer(element: PsiElement?): Tokenizer<*> = when {
+        element?.node?.elementType == RellTypes.STRING -> StringExpressionTokenizer
+        else -> super.getTokenizer(element)
+    }
+}
+
+object StringExpressionTokenizer : EscapeSequenceTokenizer<LeafPsiElement>() {
+    override fun tokenize(element: LeafPsiElement, consumer: TokenConsumer) {
+        val text = element.text
+
+        if (!text.contains("\\")) {
+            consumer.consumeToken(element, PlainTextSplitter.getInstance())
+        } else {
+            processTextWithEscapeSequences(element, text, consumer)
+        }
+    }
+
+    private fun processTextWithEscapeSequences(element: LeafPsiElement, text: String, consumer: TokenConsumer) {
+        val unescapedText = StringBuilder()
+        val offsets = IntArray(text.length + 1)
+        CodeInsightUtilCore.parseStringCharacters(text, unescapedText, offsets)
+        EscapeSequenceTokenizer.processTextWithOffsets(element, consumer, unescapedText, offsets, 1)
+    }
+}
+
