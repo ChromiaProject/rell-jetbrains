b318cffb9f4422d8343ef7d78081f7d327aabcbb
Mikheil Kezherashvili <misha1024@gmail.com>
2023-08-04 17:09:39 +0400

    Keyword completion

---
+++ src/main/kotlin/net/postchain/rellide/jetbrains/completion/KeywordCompletionContributor.kt
@@ -0,0 +1,79 @@
+package net.postchain.rellide.jetbrains.completion
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.util.ProcessingContext
+import net.postchain.rellide.jetbrains.language.RellLanguage
+import net.postchain.rellide.jetbrains.language.psi.RellTypes
+
+class KeywordCompletionContributor : CompletionContributor() {
+    init {
+        extend(
+            CompletionType.BASIC,
+            PlatformPatterns.psiElement(RellTypes.ID).withLanguage(RellLanguage.INSTANCE),
+            KeywordCompletionProvider()
+        )
+    }
+}
+
+class KeywordCompletionProvider : CompletionProvider<CompletionParameters>() {
+    private val keywords = listOf(
+        "abstract",
+        "and",
+        "break",
+        "class",
+        "continue",
+        "create",
+        "delete",
+        "else",
+        "entity",
+        "enum",
+        "false",
+        "for",
+        "function",
+        "guard",
+        "if",
+        "import",
+        "in",
+        "include",
+        "index",
+        "key",
+        "limit",
+        "list",
+        "map",
+        "module",
+        "mutable",
+        "namespace",
+        "not",
+        "null",
+        "object",
+        "offset",
+        "operation",
+        "or",
+        "override",
+        "query",
+        "record",
+        "return",
+        "set",
+        "struct",
+        "true",
+        "update",
+        "val",
+        "var",
+        "virtual",
+        "when",
+        "while"
+    )
+
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
+        keywords.forEach {
+            result.addElement(LookupElementBuilder.create(it).withBoldness(true))
+        }
+    }
+
+}
