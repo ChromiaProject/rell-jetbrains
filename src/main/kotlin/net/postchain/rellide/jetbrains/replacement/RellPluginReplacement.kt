6b3828050fd5456b822f6da913f0cf60cb58f7d0
Mikheil Kezherashvili <misha1024@gmail.com>
2023-08-31 14:31:32 +0400

    Plugin replacement
    When ultimate version will be installed IDE will suggest user to replace "plain" version

---
+++ src/main/kotlin/net/postchain/rellide/jetbrains/replacement/RellPluginReplacement.kt
@@ -0,0 +1,5 @@
+package net.postchain.rellide.jetbrains.replacement
+
+import com.intellij.ide.plugins.PluginReplacement
+
+class RellPluginReplacement : PluginReplacement("net.postchain.rellide.jetbrains.ultimate")
