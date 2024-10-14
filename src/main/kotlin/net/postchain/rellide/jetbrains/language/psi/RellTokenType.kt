13cba848573683347586eb7696c26ff4b13e11db
Mikheil Kezherashvili <misha1024@gmail.com>
2024-10-14 14:03:41 +0400

    Make compatible with Intellij Platform 242.*

--- src/main/kotlin/net/postchain/rellide/jetbrains/language/psi/RellTokenType.kt
+++ src/main/kotlin/net/postchain/rellide/jetbrains/language/psi/RellTokenType.kt
@@ -2,9 +2,8 @@ package net.postchain.rellide.jetbrains.language.psi
 
 import com.intellij.psi.tree.IElementType
 import net.postchain.rellide.jetbrains.language.RellLanguage
-import org.jetbrains.annotations.NonNls
 
-class RellTokenType(debugName: @NonNls String) :
+class RellTokenType(debugName: String) :
     IElementType(debugName, RellLanguage.INSTANCE) {
     override fun toString(): String {
         return "RellTokenType." + super.toString()
