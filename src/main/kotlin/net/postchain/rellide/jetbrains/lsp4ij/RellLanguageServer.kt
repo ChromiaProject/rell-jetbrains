066af5655598bc91201ad6bea4513e5f0c718a9c
Mikheil Kezherashvili <misha1024@gmail.com>
2025-01-16 20:42:52 +0400

    LSP 0.4.17 support for community version

--- src/main/kotlin/net/postchain/rellide/jetbrains/lsp4ij/RellLanguageServer.kt
+++ src/main/kotlin/net/postchain/rellide/jetbrains/lsp4ij/RellLanguageServer.kt
@@ -17,7 +17,7 @@ class RellLanguageServer(val project: Project) : ProcessStreamConnectionProvider
         val pluginDescriptor = PluginManagerCore.getPlugin(PluginId.getId(PLUGIN_ID))
             ?: throw IllegalStateException("Cannot find plugin by ID: $PLUGIN_ID")
         val lspJarPath = pluginDescriptor.pluginPath.toAbsolutePath()
-            .resolve("language-server/rell-language-server-0.4.16.jar")
+            .resolve("language-server/rell-language-server-0.4.17.jar")
 
         val commands = JavaProcessCommandBuilder(project, "Rell")
             .setJar(lspJarPath.toString())
