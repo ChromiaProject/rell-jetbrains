70181099bdb350d844a67a44b00a1f53dbb857a0
Mikheil Kezherashvili <misha1024@gmail.com>
2025-02-18 15:27:45 +0400

    LSP4IJ bug fixes
     - Change ProcessStreamConnectionProvider to OSProcessStreamConnectionProvider
    - Add Enable/Disable Index cache checkbox to Rell Settings
    - File change event listening is resolved from rell language server

---
+++ src/main/kotlin/net/postchain/rellide/jetbrains/settings/RellPluginSettingsConfigurable.kt
@@ -0,0 +1,44 @@
+package net.postchain.rellide.jetbrains.settings
+
+import com.intellij.openapi.options.Configurable
+import org.jetbrains.annotations.Nls
+import javax.swing.JComponent
+
+/**
+ * Provides controller functionality for Rell plugin settings.
+ */
+class RellPluginSettingsConfigurable : Configurable {
+    private var settingsComponent: RellPluginSettingsComponent? = null
+
+    // A default constructor with no arguments is required because this implementation
+    // is registered in an applicationConfigurable EP
+    override fun getDisplayName(): @Nls(capitalization = Nls.Capitalization.Title) String = "Rell Settings:"
+
+    override fun getPreferredFocusedComponent(): JComponent? {
+        return settingsComponent?.getPreferredFocusedComponent()
+    }
+
+    override fun createComponent(): JComponent? {
+        settingsComponent = RellPluginSettingsComponent()
+        return settingsComponent?.getPanel()
+    }
+
+    override fun isModified(): Boolean {
+        val settings = RellPluginSettingsState.instance
+        return settings.indexCaching != settingsComponent?.indexCachingState
+    }
+
+    override fun apply() {
+        val settings = RellPluginSettingsState.instance
+        settings.indexCaching = settingsComponent?.indexCachingState ?: false
+    }
+
+    override fun reset() {
+        val settings = RellPluginSettingsState.instance
+        settingsComponent?.indexCachingState = settings.indexCaching
+    }
+
+    override fun disposeUIResources() {
+        settingsComponent = null
+    }
+}
