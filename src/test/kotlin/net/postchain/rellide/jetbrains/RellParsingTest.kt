ea729f53a4f15683afb58a1c7726e1409b198008
Tim Steinholtz <tim.steinholtz@chromaway.com>
2025-11-24 17:51:06 +0100

    Harmonize line separators in parsing test

--- src/test/kotlin/net/postchain/rellide/jetbrains/RellParsingTest.kt
+++ src/test/kotlin/net/postchain/rellide/jetbrains/RellParsingTest.kt
@@ -3,16 +3,31 @@ package net.postchain.rellide.jetbrains
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.intellij.psi.PsiFile
 import com.intellij.testFramework.ParsingTestCase
 import com.intellij.testFramework.ParsingTestUtil
-import com.jetbrains.rd.util.printlnError
 import net.postchain.rellide.jetbrains.language.RellParserDefinition
 import java.io.File
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class RellTestCaseSnippet(val files: Map<String, String>, val parsing: Map<String, List<Map<String, String>>>)
 
-class RellParsingTest : ParsingTestCase("", "rell", true, RellParserDefinition()) {
+
+open class RellParsingTestCase: ParsingTestCase("", "rell", true, RellParserDefinition()) {
+    override fun parseFile(name: String?, text: String?): PsiFile {
+        /* assertValidSeparators() in com.intellij.openapi.editor.impl.DocumentImpl doesn't accept '\r'.
+        The following two resources state that Document don't accepts '\r'.
+        Document is Intellij internal abstract users text file. User can still set \r to be their preferred line-separator in the text file.
+        https://plugins.jetbrains.com/docs/intellij/modifying-psi.html#creating-the-new-psi
+        https://plugins.jetbrains.com/docs/intellij/documents.html#what-are-the-rules-of-working-with-documents
+        */
+        val normalizedLineSeparator = text?.replace("\r\n", "\n")?.replace("\r", "\n")
+        return super.parseFile(name, normalizedLineSeparator)
+    }
+}
+
+
+class RellParsingTest : RellParsingTestCase() {
 
     override fun getTestDataPath() = "build/rell-test-cases/test-cases"
 
